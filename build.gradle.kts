34d2ff8cc9e1e950cc895975de2de0f8b4ca633f
hytejasvi <hytejasvi@gmail.com>
2025-08-29 11:31:36 +0200

    Project setup and base config

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.5.5"
+	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("plugin.jpa") version "1.9.25"
+}
+
+group = "com.codefactory"
+version = "0.0.1-SNAPSHOT"
+description = "Url Shortener Service"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	// Arrow: functional programming library for Kotlin
+	//implementation(platform("io.arrow-kt:arrow-stack:1.2.4"))
+	//implementation("io.arrow-kt:arrow-core")
+
+	// Spring Boot starters
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	// Database
+	runtimeOnly("com.h2database:h2")
+
+	// Kotlin
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// Utils
+	implementation("commons-validator:commons-validator:1.7")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+
+	// Tests
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+allOpen {
+	annotation("jakarta.persistence.Entity")
+	annotation("jakarta.persistence.MappedSuperclass")
+	annotation("jakarta.persistence.Embeddable")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/codefactrory/shorty/ShortyApplication.kt
@@ -0,0 +1,11 @@
+package com.codefactrory.shorty
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShortyApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShortyApplication>(*args)
+}
